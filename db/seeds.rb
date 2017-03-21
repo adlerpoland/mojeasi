797893607cc304e05a9380f26b2c878a55f74c49
AdlerPoland <resepoland@o2.pl>
2017-03-21 14:59:28 +0100

    seeds.rb added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+seed_photos = [
+    ['Coffe','Small coffee first in the morning'],
+    ['Coffee river','Find your own way out, don not let anyone stop you'],
+    ['Fruits','Taste this fresh fruits'],
+    ['Olds','Blue rocket'],
+    ['Classic','Classic cars are best'],
+    ['Money','Work your life to get it..'],
+    ['An apple','just a single apple'],
+    ['Brown coffee','Taste it'],
+    ['Helicopter','...an aircraft whose lift is derived from the aerodynamic forces acting on one or more powered rotors turning about substantially vertical axes']
+    ]
+
+seed_photos.each do |title,description|
+    Photos.create(title: title,description:description)
+end

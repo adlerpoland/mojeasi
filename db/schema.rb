58f5c720cc3e6eee1c2516d2ea98ff98c8b70366
RGolun <rgolunski@sigma.ug.edu.pl>
2017-04-30 00:54:55 -0700

    Add comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170319210104) do
+ActiveRecord::Schema.define(version: 20170430075107) do
 
   create_table "comments", force: :cascade do |t|
-    t.string   "user_name"
-    t.text     "body"
+    t.text     "content"
     t.integer  "photo_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["photo_id"], name: "index_comments_on_photo_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "photos", force: :cascade do |t|

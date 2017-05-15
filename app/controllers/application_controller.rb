da03fdaedb810f7aa14d4dc2d74bb58e943f683a
adlerpoland <resepoland@o2.pl>
2017-05-15 17:56:24 +0200

    redirect to https

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!, :except => [:show, :index,:info]
+  def redirect_to_https
+    redirect_to :protocol => "https://" unless (request.ssl? || request.local?)
+  end
+  before_filter :redirect_to_https
 end
